9f9620e7ae5f31fc72632aa2ce2a060382ba4dc7
Steffen Lehmann <steffen@stejin.org>
2025-01-01 14:55:41 -0800

    Updated invoices

--- Data/Invoice.cs
+++ Data/Invoice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Data;
 
@@ -26,4 +27,12 @@ public partial class Invoice
     public virtual Customer Customer { get; set; } = null!;
 
     public virtual ICollection<InvoiceItem> InvoiceItems { get; set; } = new List<InvoiceItem>();
+
+    public string ItemList() {
+        StringBuilder result = new StringBuilder();
+        InvoiceItems.ToList().ForEach(i => result.Append(i.Track.Name).Append(", "));
+        if (result.Length > 2)
+            result.Remove(result.Length - 2, 2);
+        return result.ToString();
+    }
 }
